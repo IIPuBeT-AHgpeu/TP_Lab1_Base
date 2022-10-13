861850dbf7b4fee7660ec3dcabc6dfa7a4bf7aa0
Kirpa <kiran2000@yandex.ru>
2022-10-13 11:06:25 +0300

    Добавил класс с текстовыми форматами.

---
+++ TP_Lab1_Base/Service/DataReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Lab1_Base.Service
+{
+    internal class DataReader
+    {
+        
+        public string FileName { get; set; }
+    }
+}
---
+++ TP_Lab1_Base/Service/TextFileFormat.cs
@@ -0,0 +1,26 @@
+﻿namespace TP_Lab1_Base.Service
+{
+    internal class TextFileFormat
+    {
+        public TextFormat Format { get; set; }
+
+        public string? GetUpperFormatName()
+        {
+            return Enum.GetName(typeof(TextFormat), Format);
+        }
+
+        public string? GetLowerFormatName()
+        {
+            string? result = Enum.GetName(typeof(TextFormat), Format);
+            if (result == null) return null;
+            else return result.ToLower();
+        }
+
+        public enum TextFormat
+        {
+            TXT,
+            JSON,
+            XML
+        }
+    }
+}
