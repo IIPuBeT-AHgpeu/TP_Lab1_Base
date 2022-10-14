a2519de3cba9c69f9f560eafd61e9e4f6c568372
Kirpa <kiran2000@yandex.ru>
2022-10-14 10:57:12 +0300

    Реализовал класс-парсер MyParser.

--- TP_Lab1_Base/Main.cs
+++ TP_Lab1_Base/Main.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TP_Lab1_Base.Models;
 using TP_Lab1_Base.Service;
 
 FileReader dataReader = new FileReader("StandartTypeData.txt", @"C:\Users\Kirpa\source\repos\TP_Lab1_Base\TP_Lab1_Base\Data");
 
-Console.WriteLine(Directory.GetCurrentDirectory());
-
 MyParser parser = new MyParser();
-Console.WriteLine(dataReader.ReadStringFromFile());
-parser.TryParse(dataReader.ReadStringFromFile());
 
+StandartModel[] students = parser.TryParse(dataReader.ReadStringFromFile());
+
+Console.WriteLine(students.Length);
--- TP_Lab1_Base/Service/MyParser.cs
+++ TP_Lab1_Base/Service/MyParser.cs
@@ -7,15 +7,80 @@ namespace TP_Lab1_Base.Service
     {
         public StandartModel[]? Parse(string data)
         {
-            throw new NotImplementedException();
+            data = data.Replace("\r", "");
+
+            var chunks = Regex.Matches(data, CHUNK_REGEX, RegexOptions.Multiline);
+
+            if (chunks.Count == 0) return null;
+
+            List<StandartModel> students = new List<StandartModel>();
+
+            foreach (Match chunk in chunks)
+            {
+                StandartModel student = new StandartModel();
+                string[] parsedChunk = chunk.Value.Split('\n');
+
+                if (parsedChunk.Length < 2) return null;
+
+                student.Name = parsedChunk[0];
+
+                List<EducationalSubject> subjects = new List<EducationalSubject>();
+                for (int i = 1; i < parsedChunk.Length && parsedChunk[i] != ""; i++)
+                {
+                    if (parsedChunk[i][0] == ' ')
+                    {
+                        string[] subjNameSubjScoreString = parsedChunk[i].Split(':');
+
+                        if (subjNameSubjScoreString.Length < 2) return null;
+
+                        subjects.Add(new EducationalSubject() { Name = subjNameSubjScoreString[0].Remove(0), Score = Int32.Parse(subjNameSubjScoreString[1]) });
+                    }
+                }
+
+                student.EducationalSubjects = subjects.ToArray();
+                students.Add(student);
+            }
+
+            return students.ToArray();
         }
 
         public StandartModel[] TryParse(string data)
-        {           
-            Regex regex = new Regex(CHUNK_REGEX, RegexOptions.Multiline);
-            Console.WriteLine(regex.Matches(data).Count);
+        {
+            data = data.Replace("\r", "");
 
-            return null;
+            var chunks = Regex.Matches(data, CHUNK_REGEX, RegexOptions.Multiline);
+
+            if (chunks.Count == 0) throw new Exception("Have no matches!");
+
+            List<StandartModel> students = new List<StandartModel>();
+
+            foreach (Match chunk in chunks)
+            {
+                StandartModel student = new StandartModel();
+                string[] parsedChunk = chunk.Value.Split('\n');
+
+                if (parsedChunk.Length < 2) throw new Exception("Invalid parsed data!");
+
+                student.Name = parsedChunk[0];
+
+                List<EducationalSubject> subjects = new List<EducationalSubject>();
+                for (int i = 1; i < parsedChunk.Length && parsedChunk[i] != ""; i++)
+                {
+                    if (parsedChunk[i][0] == ' ')
+                    {
+                        string[] subjNameSubjScoreString = parsedChunk[i].Split(':');
+
+                        if (subjNameSubjScoreString.Length < 2) throw new Exception("Invalid parsed \"subject:score\" string!");
+
+                        subjects.Add(new EducationalSubject() { Name = subjNameSubjScoreString[0].Remove(0), Score = Int32.Parse(subjNameSubjScoreString[1]) });
+                    }
+                }
+
+                student.EducationalSubjects = subjects.ToArray();
+                students.Add(student);
+            }
+
+            return students.ToArray();
         }
 
         private static readonly string CHUNK_REGEX = @"[а-яА-Я a-zA-Z-.]+\n((\t| )[a-zA-Zа-яА-Я-.]+:[0-9]{1,3}(\n|$))+";
