75b4d398b32cba1c11568f3fa3d040dd4acdd0a8
Kirpa <kiran2000@yandex.ru>
2022-11-11 14:33:42 +0300

    Реализовал функцию main.

--- TP_Lab1_Base/Main.cs
+++ TP_Lab1_Base/Main.cs
@@ -1,55 +1,61 @@
 ﻿using TP_Lab1_Base.Models;
 using TP_Lab1_Base.Service;
 
-//Console.WriteLine("Введите путь к файлу:");
-//string path = Console.ReadLine();
-//Console.WriteLine("Введите имя файла (включая расширение файла)");
-//string fileName = Console.ReadLine();
+Console.WriteLine("Выберите парсер:\n1 - парсер стартового формата (формат предоставил преподаватель);\n2 - парсер XML.");
 
-//FileReader dataReader = new FileReader(fileName, path);
+const string STANDART_TYPE_FILENAME = "StandartTypeData{NUMBER}.txt";
+const string XML_TYPE_FILENAME = "XMLTypeData{NUMBER}.xml";
 
-//while(!dataReader.FileIsExist)
-//{
-//    Console.WriteLine("Введите путь к файлу:");
-//    path = Console.ReadLine();
-//    Console.WriteLine("Введите имя файла (включая расширение файла)");
-//    fileName = Console.ReadLine();
+string input_buffer = "";
+int mode = 0;
+string fileName;
+FileReader reader;
+IParser<Student[]> parser;
 
-//    dataReader = new FileReader(fileName, path);
-//}
+input_buffer = Console.ReadLine();
+if (input_buffer == "1" || input_buffer == "2")
+{
+    mode = Int32.Parse(input_buffer);
 
-//MyParser parser = new MyParser();
+    Console.WriteLine("Выберите набор данных:" +
+            "\n - введите \"E\" (без кавычек), для получения расширенного набор (5 записей);" +
+            "\n - введите любую другую строку для получения стандартного набора.");
+    input_buffer = Console.ReadLine();
 
-//Student[] students = parser.TryParse(dataReader.ReadStringFromFile());
+    if (mode == 1)
+    {
+        if (input_buffer == "E") fileName = STANDART_TYPE_FILENAME.Replace("{NUMBER}", "2");
+        else fileName = STANDART_TYPE_FILENAME.Replace("{NUMBER}", "1");
 
-//foreach (var student in students)
-//{
-//    Console.WriteLine(student.Name + ", средний балл:");
-//    Console.WriteLine(Calculator.GetAverageScore(student));
-//}
+        parser = new MyParser();
+    }
+    else
+    {
+        if (input_buffer == "E") fileName = XML_TYPE_FILENAME.Replace("{NUMBER}", "2");
+        else fileName = XML_TYPE_FILENAME.Replace("{NUMBER}", "1");
 
-//dataReader = FileReader.OpenFileFromProject(fileName);
+        parser = new XMLParser();
+    }
 
-//foreach (var student in students)
-//{
-//    Console.WriteLine(student.Name + ", средний балл:");
-//    Console.WriteLine(Calculator.GetAverageScore(student));
-//}
+    reader = FileReader.OpenFileFromProject(fileName);
+    string data = reader.ReadStringFromFile();
 
-string fileName = @"XMLTypeData2.xml";
-FileReader reader = FileReader.OpenFileFromProject(fileName);
+    Student[] group = parser.Parse(data);
 
-string data = reader.ReadStringFromFile();
-Console.WriteLine(data);
+    Console.WriteLine("Количество студентов в группе: {0}", group.Length);
+    Console.WriteLine("Состав группы: ");
 
-Console.WriteLine("--------------------------------------------------");
-fileName = @"XMLTypeData1.xml";
-reader = FileReader.OpenFileFromProject(fileName);
-data = reader.ReadStringFromFile();
-Console.WriteLine(data);
-//XMLParser parser = new XMLParser();
+    foreach (Student student in group)
+    {
+        Console.WriteLine("ФИО: {0}, средний балл: {1}", student.Name, Calculator.GetAverageScore(student));
+    }
+    Console.WriteLine("Количество хорошистов группы: {0}", Calculator.GetNumberOfGoodies(group));
 
-////Act
-//Student[] parsedData = parser.Parse(data);
-
-Console.ReadLine();
+    Console.WriteLine("-----------------------------------------------------------");
+    Console.WriteLine("Исходник: \n{0}", data);
+    Console.WriteLine("-----------------------------------------------------------");
+}
+else
+{
+    Console.WriteLine("Неккоректный ввод. Программа завершает работу.");
+}
