d532a0bd6c9b43ee190713b4e59a11a930e14d42
Kirpa <kiran2000@yandex.ru>
2022-11-11 11:51:01 +0300

    Изменил уровень доступа у класса FileReader. Добавил тесты для XML-парсера.

--- TP_Lab1_Base/Main.cs
+++ TP_Lab1_Base/Main.cs
@@ -1,37 +1,47 @@
 ﻿using TP_Lab1_Base.Models;
 using TP_Lab1_Base.Service;
 
-Console.WriteLine("Введите путь к файлу:");
-string path = Console.ReadLine();
-Console.WriteLine("Введите имя файла (включая расширение файла)");
-string fileName = Console.ReadLine();
+//Console.WriteLine("Введите путь к файлу:");
+//string path = Console.ReadLine();
+//Console.WriteLine("Введите имя файла (включая расширение файла)");
+//string fileName = Console.ReadLine();
 
-FileReader dataReader = new FileReader(fileName, path);
+//FileReader dataReader = new FileReader(fileName, path);
 
-while(!dataReader.FileIsExist)
-{
-    Console.WriteLine("Введите путь к файлу:");
-    path = Console.ReadLine();
-    Console.WriteLine("Введите имя файла (включая расширение файла)");
-    fileName = Console.ReadLine();
+//while(!dataReader.FileIsExist)
+//{
+//    Console.WriteLine("Введите путь к файлу:");
+//    path = Console.ReadLine();
+//    Console.WriteLine("Введите имя файла (включая расширение файла)");
+//    fileName = Console.ReadLine();
 
-    dataReader = new FileReader(fileName, path);
-}
+//    dataReader = new FileReader(fileName, path);
+//}
 
-MyParser parser = new MyParser();
+//MyParser parser = new MyParser();
 
-Student[] students = parser.TryParse(dataReader.ReadStringFromFile());
+//Student[] students = parser.TryParse(dataReader.ReadStringFromFile());
 
-foreach (var student in students)
-{
-    Console.WriteLine(student.Name + ", средний балл:");
-    Console.WriteLine(Calculator.GetAverageScore(student));
-}
+//foreach (var student in students)
+//{
+//    Console.WriteLine(student.Name + ", средний балл:");
+//    Console.WriteLine(Calculator.GetAverageScore(student));
+//}
 
-dataReader = FileReader.OpenFileFromProject(fileName);
+//dataReader = FileReader.OpenFileFromProject(fileName);
 
-foreach (var student in students)
-{
-    Console.WriteLine(student.Name + ", средний балл:");
-    Console.WriteLine(Calculator.GetAverageScore(student));
-}
+//foreach (var student in students)
+//{
+//    Console.WriteLine(student.Name + ", средний балл:");
+//    Console.WriteLine(Calculator.GetAverageScore(student));
+//}
+
+string fileName = @"XMLTypeData1.xml";
+FileReader reader = FileReader.OpenFileFromProject(fileName);
+
+string data = reader.ReadStringFromFile();
+Console.WriteLine(data);
+XMLParser parser = new XMLParser();
+
+//Act
+Student[] parsedData = parser.Parse(data);
---
+++ TP_Lab1_Base_Tests/XMLParserTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP_Lab1_Base.Models;
+using TP_Lab1_Base.Service;
+
+namespace TP_Lab1_Base_Tests
+{
+    [TestClass]
+    public class XMLParserTests
+    {
+        [TestMethod]
+        public void XMLParseTest_file_1()
+        {
+            //Arrange
+            string fileName = @"XMLTypeData1.xml";
+            FileReader reader = FileReader.OpenFileFromProject(fileName);
+
+            string data = reader.ReadStringFromFile();
+            XMLParser parser = new XMLParser();
+
+            //Act
+            Student[] parsedData = parser.Parse(data);
+            bool isCorrect = true;
+
+            if (parsedData.Length != 3) isCorrect = false;
+            else
+            {
+                if (parsedData[0] == null) isCorrect = false;
+                else
+                {
+                    if (parsedData[parsedData.Length - 1].Name != "Соколов Михаил Сергеевич") isCorrect = false;
+                }
+            }
+
+            //Assert
+            Assert.IsTrue(isCorrect);
+        }
+
+        [TestMethod]
+        public void XMLParseTest_file_2()
+        {
+            //Arrange
+            string fileName = @"XMLTypeData2.xml";
+            FileReader reader = FileReader.OpenFileFromProject(fileName);
+
+            string data = reader.ReadStringFromFile();
+            XMLParser parser = new XMLParser();
+
+            //Act
+            Student[] parsedData = parser.Parse(data);
+            bool isCorrect = true;
+            string message = "";
+
+            if (parsedData.Length != 5) isCorrect = false;
+            else
+            {
+                if (parsedData[0] == null) isCorrect = false;
+                else
+                {
+                    if (parsedData[2].EducationalSubjects.Length != 5) isCorrect = false;
+                    else
+                    {
+                        if (parsedData[parsedData.Length - 1].Name != "Вак Михаил Сергеевич") isCorrect = false;
+                    }
+                }
+            }
+
+            //Assert
+            Assert.IsTrue(isCorrect);
+        }
+    }
+}
