e20d87a77b0b88062be89d70302579855845ad15
Kirpa <kiran2000@yandex.ru>
2022-11-11 13:44:59 +0300

    Убрал лишние зависимости. Реализовал функцию определения количества хорошистов.

--- TP_Lab1_Base_Tests/XMLParserTests.cs
+++ TP_Lab1_Base_Tests/XMLParserTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TP_Lab1_Base.Models;
+﻿using TP_Lab1_Base.Models;
 using TP_Lab1_Base.Service;
 
 namespace TP_Lab1_Base_Tests
